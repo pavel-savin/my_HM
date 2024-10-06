8491f39eeedf9bf099c4aec0aadb5f9299bce7a2
Pavel savin <17.115@mail.ru>
2024-10-07 00:46:12 +0300

    first commit

---
+++ New_HM/my_HM/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    categories = models.CharField(max_length=100, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="post")
+    article_or_news = models.IntegerField(default=0)
+    automatic_data_time = models.DateTimeField(auto_now_add=True)
+    post_category = models.ManyToManyField(Category, through="Post_Category")
+    article_title_news = models.CharField(
+        max_length=255, db_index=True
+    )  # добавлено индексирование
+    text_title_news = models.TextField()
+    rating = models.IntegerField(default=0)
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
+    automatic_data_time = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
