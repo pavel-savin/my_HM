fe5bcff8c6df835b5b968f2117d2d45fbce4ac2f
Pavel savin <17.115@mail.ru>
2024-10-07 02:01:47 +0300

    without using aggregation functions

--- New_HM/my_HM/models.py
+++ New_HM/my_HM/models.py
@@ -2,11 +2,32 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Author(models.Model):
-    user = models.OneToOneField(User, on_delete= models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
-    posts = models.Manager
-    
-    
+
+    def update_rating(self):
+        posts = Post.objects.filter(author=self)  # Получаем все статьи автора
+        post_rating_sum = 0
+
+        for post in posts:  # Суммируем рейтинг всех статей
+            post_rating_sum += post.rating
+
+        post_rating_sum *= 3
+
+        comments_by_author = Comment.objects.filter(user=self.user)  # Суммируем рейтинг всех комментариев, оставленных автором
+        comment_author_rating_sum = 0
+        for comment in comments_by_author:
+            comment_author_rating_sum += comment.rating
+
+        comments_to_posts = Comment.objects.filter(post__author=self)  # Суммируем рейтинг всех комментариев к статьям автора
+        comment_to_posts_rating_sum = 0
+        for comment in comments_to_posts:
+            comment_to_posts_rating_sum += comment.rating
+
+        self.rating = post_rating_sum + comment_author_rating_sum + comment_to_posts_rating_sum  # Общий рейтинг
+        self.save()
+
+
         
 class Category(models.Model):
     categories = models.CharField(max_length=100, unique=True)
